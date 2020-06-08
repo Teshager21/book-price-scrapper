376939ebbd5ec356b39bb5da7d61098e623ee61d
teshager21 <teshager8922@gmail.com>
2020-06-08 02:51:55 -0600

    add test for #parsed_page

--- spec/page_parser_spec.rb
+++ spec/page_parser_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe PageParser do
   let(:page_parser) { PageParser.new }
 
   describe '#url' do
-    it 'returns has a setter and getter for the url attribute ' do
+    it 'has a setter and getter for the url attribute ' do
       page_parser.url = 'https://www.amazon.com/Best-Sellers-Books-Deals/zgbs/books/45/ref=zg_bs_pg_1?_encoding=UTF8&pg=1'
       expect(page_parser.url).to eql('https://www.amazon.com/Best-Sellers-Books-Deals/zgbs/books/45/ref=zg_bs_pg_1?_encoding=UTF8&pg=1')
     end
@@ -14,6 +14,11 @@ RSpec.describe PageParser do
     it 'returns a none nil page for a valid url' do
       page_parser.url = 'https://www.amazon.com/Best-Sellers-Books-Deals/zgbs/books/45/ref=zg_bs_pg_1?_encoding=UTF8&pg=1'
       expect(page_parser.bring_unparsed_page).not_to eql(nil)
+    end
+  end
+  describe '#parsed_page' do
+    it 'has a getter for the parsed_page attribute' do
+      expect(page_parser.parsed_page).not_to eql(nil)
     end
   end
 end
