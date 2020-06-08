208cf2f342cb65f1477083c1990a9006b9dc3889
teshager21 <teshager8922@gmail.com>
2020-06-08 05:23:31 -0600

    create a data_filter file and its test

---
+++ lib/data_filter.rb
@@ -0,0 +1,4 @@
+require_relative('page_parser')
+
+class DataFilter
+end
---
+++ spec/data_filter_spec.rb
@@ -0,0 +1,7 @@
+require 'rspec'
+require 'data_filter'
+
+RSpec.describe DataFilter do
+  it 'has a getter for the document' do
+  end
+end
