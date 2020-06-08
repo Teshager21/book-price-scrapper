6eb7d080582e578aa5b441542d72f559a1a3e8a3
teshager21 <teshager8922@gmail.com>
2020-06-08 05:50:30 -0600

    write test for initialize method

--- lib/data_filter.rb
+++ lib/data_filter.rb
@@ -1,4 +1,5 @@
 require_relative('page_parser')
 
 class DataFilter
+  attr_writer :document
 end
--- spec/data_filter_spec.rb
+++ spec/data_filter_spec.rb
@@ -1,10 +1,22 @@
 require 'rspec'
 require 'data_filter'
+require 'page_parser'
 
 RSpec.describe DataFilter do
-  data_filter = DataFilter.new
+  let(:data_filter) { DataFilter.new }
+  let(:doc) do
+    page_parser = PageParser.new('https://www.amazon.com/Best-Sellers-Books
+   -Deals/zgbs/books/45/ref=zg_bs_pg_1?_encoding=UTF8&pg=1')
+    page_parser.parse_page
+    page_parser.parsed_page
+  end
   describe '#document'
   it 'has a getter for the document attribute' do
+    expect(data_filter.document).not_to eql(nil)
+  end
+  describe '#initialize'
+  it 'has an intialize method that sets the document attribute' do
+    data_filter = DataFilter.new(doc)
     expect(data_filter.document).not_to eql(nil)
   end
 end
