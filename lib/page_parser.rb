d044ecdd7ee5abced5a32cc3762186f87266f773
teshager21 <teshager8922@gmail.com>
2020-06-09 12:57:07 -0600

    draft readme

--- lib/page_parser.rb
+++ lib/page_parser.rb
@@ -1,14 +1,9 @@
 require 'httparty'
 require 'nokogiri'
 class PageParser
-  attr_accessor :url
   attr_reader :parsed_page, :unparsed_page
-  def bring_unparsed_page
-    @unparsed_page = HTTParty.get(@url)
-  end
 
   def parse_page
-    bring_unparsed_page
     @parsed_page = Nokogiri::HTML(@unparsed_page.body)
   end
 
@@ -17,8 +12,7 @@ class PageParser
   end
 
   def initialize(url)
-    @url = url
     @parsed_page = nil
-    @unparsed_page = nil
+    @unparsed_page = HTTParty.get(url)
   end
 end
